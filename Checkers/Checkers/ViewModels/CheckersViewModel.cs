eccbe24c8632df4739cc1266069c86bf6120d051
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2024-03-20 18:01:16 +0100

    updated viewmodel

--- Checkers/Checkers/ViewModels/CheckersViewModel.cs
+++ Checkers/Checkers/ViewModels/CheckersViewModel.cs
@@ -10,35 +10,39 @@ namespace Checkers.ViewModels
     {
         private Tile _selectedTile;
         public Board Board { get; set; }
-        public Player PlayerTurn { get; }
-        public Player Player1 { get; set; }
-        public Player Player2 { get; set; }
+        private Player PlayerTurn { get; set; }
+        private Player Player1 { get; }
+        private Player Player2 { get; }
 
         public CheckersViewModel()
         {
             this.Board = new Board();
             this.Board.InitializeBoard();
             this._selectedTile = null;
-            this.Player1 = new Player(true, "player1");
-            this.PlayerTurn = new Player(false, "player2");
+            this.Player1 = new HumanPlayer(true, "player1");
+            this.Player2 = new HumanPlayer(false, "player2");
+            this.PlayerTurn = this.Player1;
         }
 
         [ICommand]
         public void SelectTile(Tile tile)
         {
-            if (tile.Color == Colors.White) return;
-
+            if (tile.Color.Equals(Colors.White)) return;
+            var playerColor = this.PlayerTurn.IsWhite ? Colors.White : Colors.Black;
+            
             if (this._selectedTile != null && tile.IsHighlighted())
             {
                 tile.Piece.Show(this._selectedTile.Piece.Color);
                 this._selectedTile.Piece.Hide();
                 this.Board.ResetHighlightedTiles();
+                this.PlayerTurn = this.PlayerTurn == Player1 ? Player2 : Player1;
             }
             else
             {
+                if (!this.PlayerTurn.Color.Equals(tile.Piece.Color)) return ;
                 if (!tile.HasPiece()) return;
                 this._selectedTile = tile;
-                Board.ShowPossibleMoves(this._selectedTile);
+                Board.ShowPossibleMoves(this._selectedTile, this.PlayerTurn);
                 tile.Color = Colors.Green;
             }
 
@@ -50,7 +54,5 @@ namespace Checkers.ViewModels
         {
             this.Board.Reset();
         }
-
-        
     }
 }
