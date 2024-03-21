19e2d7a51caa15026367e96f71c3633b7f7a24e4
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2024-03-21 23:30:35 +0100

    styling change

--- Checkers/Checkers/AppColors.cs
+++ Checkers/Checkers/AppColors.cs
@@ -2,10 +2,10 @@
 
 public static class AppColors
 {
-    public static Color WhiteTile { get; set; } = Color.FromRgb(239, 233, 227);
-    public static Color BlackTile { get; set; } = Color.FromRgb(121, 175, 129);
-    public static Color WhitePiece { get; set; } = Color.FromRgb(238, 223, 187);
-    public static Color BlackPiece { get; set; } = Color.FromRgb(206, 61, 70);
-    public static Color SelectedTile { get; set; } = Color.FromRgb(85, 124, 91);
-    public static Color HighlightedTile { get; set; } = Color.FromRgb( 85, 124, 91);
+    public static Color WhiteTile { get; set; } = Color.FromRgb(240, 217, 178);
+    public static Color BlackTile { get; set; } = Color.FromRgb(181, 136, 96);
+    public static Color WhitePiece { get; set; } = Colors.White;
+    public static Color BlackPiece { get; set; } = Colors.Black;
+    public static Color SelectedTile { get; set; } = Color.FromRgb(169, 169, 169);
+    public static Color HighlightedTile { get; set; } = Color.FromRgb( 105, 105, 105);
 }
