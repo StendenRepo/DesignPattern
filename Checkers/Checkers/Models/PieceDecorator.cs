30d7e93801a7b0c3380a998c599b90f4aa9bd791
Max Appeldorn <mappeldorn1@st.noorderpoort.nl>
2024-03-25 15:19:16 +0100

    added pieceDecorator

--- Checkers/Checkers/Models/PieceDecorator.cs
+++ Checkers/Checkers/Models/PieceDecorator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Checkers.CheckersLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Checkers.Models
 {
-    internal class PieceDecorator
+    public abstract class PieceDecorator
     {
+        Piece piece;
+        protected PieceDecorator(Piece piece)
+        {
+            this.piece = piece;
+        }
     }
 }
